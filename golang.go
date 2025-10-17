4d34190cd530c9d845c1055421cc76b221dd8ced
himateja27 <himatejahima032@gmail.com>
2025-10-17 12:35:32 +0530

    mesaage

--- golang.go
+++ golang.go
@@ -1,7 +1,74 @@
 package main
 
 import "fmt"
+
+//func wish(){
+//	fmt.Println("have a nice day")
+//}
+'''func add(a int,b int){
+	fmt.Println(a+b)
+}
+func wish(name string){
+	var message="hello"
+	fmt.Println(message,"good morning",name)
+}'''
 func main(){
-	fmt.Println(12+9)
-	fmt.Print("this is my first golang program")
+	//const(
+	//	pie=3.14
+	//	status=true
+	//)
+	//const name ="teja"
+	//var name,location string= "teja ", " hyd"
+	//var age,dob int= 21, 2001
+	//name:="teja"
+	//age:=90
+	//dob:=2001
+	//web:="www.google.com"
+	//var name="teja"
+	//var age =23
+	//var age int
+	//var name string
+	//age=30
+	//name="ranga"
+	//var rollno int = 45
+	//var marks float32=68.56
+	//var name string="teja"
+	//var isactive bool=true
+	//var name1:="teja"
+	//var age=23
+	//var name="teja"
+	//fmt.Print(name," ",age,"\n")
+	//fmt.Printf("the value of %v the type of %T",age,name)
+	//fmt.Println(name,age)
+	//fmt.Print("this is my first golang program")
+	//fmt.Print("your rollno is :", rollno, marks, name, isactive)
+	//age =70
+	//fmt.Print(name , age, dob , web)
+	//fmt.Print(name,age,dob,location)
+	//fmt.Print(name)
+	//fmt.Print(pie,status)
+	//a :="hello "
+	//b:="world "
+	//num1:=1
+	//num2:=2
+	//fmt.Print(a,b)
+	//fmt.Print("\n",num1,num2)
+	//a:="hello"
+	//b:="world"
+	//fmt.Print(a)
+	//fmt.Print(b)
+	//fmt.Println(a)
+	//fmt.Print(b)
+	//a:=10
+	//fmt.Printf("this is value of %v and type of %T",a,a)
+	//a:=10
+	//b:=20
+	//fmt.Println(a+b)
+	//wish()
+	'''add(10,20)
+	var a="teja"
+	var b="guna"
+	wish(a)
+	wish(b)'''
+	
 }
